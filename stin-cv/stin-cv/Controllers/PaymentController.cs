392ffd0d280eb6b7524cc5a9ff3c05bf91cf5102
Jan_Reisiegel <jan.reisiegel@tul.cz>
2024-03-21 14:32:14 +0100

    Copy from github

---
+++ stin-cv/stin-cv/Controllers/PaymentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StinCVProject.Models;
+
+namespace StinCVProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok("Hello world from PaymentController");
+        }
+
+        [HttpGet("time")]
+        public IActionResult Time()
+        {
+            return Ok(DateTime.Now);
+        }
+
+        [HttpPost("pay")]
+        public IActionResult Pay([FromBody] Payment payment)
+        {
+            if (payment.PaymentType == PaymentType.Cash)
+            {
+                return Ok(CardPay(payment.Amount, payment.Currency));
+            }
+            else if (payment.PaymentType == PaymentType.Card)
+            {
+                return Ok(CashPay(payment.Amount, payment.Currency));
+            }
+            return Ok(payment);
+        }
+        private string CardPay(double castka, string mena)
+        {
+            return ("Nevim jak mam platit");
+        }
+        private string CashPay(double amount, string currency)
+        {
+            return $"Zaplaceno {amount} {currency}";
+        }
+    }
+    
+}
---
+++ stin-cv/stin-cv/Models/Payment.cs
@@ -0,0 +1,17 @@
+﻿namespace StinCVProject.Models
+{
+    public class Payment
+    {
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+        public DateTime Date { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public List<string> Items { get; set; }
+
+    }
+    public enum PaymentType
+    {
+        Cash,
+        Card,
+    }
+}
