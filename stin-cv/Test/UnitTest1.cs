7957a293d179fdddf0ccb9f50a4f1450dcae28ee
Jan_Reisiegel <jan.reisiegel@tul.cz>
2024-03-22 12:55:52 +0100

    Test

--- stin-cv/Test/UnitTest1.cs
+++ stin-cv/Test/UnitTest1.cs
@@ -5,13 +5,24 @@ namespace Test
     [TestClass]
     public class UnitTest1
     {
+        private readonly HttpClient _client;
 
-        PaymentController controller = new PaymentController();
+        public UnitTest1()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7181")
+            };
+        }
+
         [TestMethod]
         public void TestHelloWorld()
         {
-            var result = controller.Hello();
-            Assert.AreEqual("Hello World", result.Value);
+            var response = _client.GetAsync(_client.BaseAddress + "api/Payment/hello").Result;
+            Assert.IsTrue(response.IsSuccessStatusCode);
+            Assert.AreEqual("Hello World", response.Content.ReadAsStringAsync().Result);
         }
+
+
     }
 }
